e39b52d8220d8d373dca16ddb284822b308fc7cd
tejalsharma2023 <teejalsharma@aol.com>
2025-09-08 13:58:16 +0530

    Create HoenScannerConfiguration.java

---
+++ src/main/resources/src/main/resources/src/main/java/com/skyscanner/hoenscannerexercise/HoenScannerConfiguration.java
@@ -0,0 +1,8 @@
+package com.skyscanner.hoenscannerexercise;
+
+import io.dropwizard.Configuration;
+
+public class HoenScannerConfiguration extends Configuration {
+    // This class can be used to configure your application
+    // For this simple example, we don't need any custom configuration
+}
